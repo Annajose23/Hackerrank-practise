c9fdf598a72207a1beed38cd1b89ffc378f6f1b3
Anna Jose <joann23july@gmail.com>
2020-05-07 00:07:50 -0400

    Ransom Note

---
+++ javascript/RansomNote.js
@@ -0,0 +1,26 @@
+function checkMagazine(magazine, note) {
+   const magazineArray = magazine.split(" ");
+   const noteArray = note.split(" ");
+  console.log(magazineArray);
+   const magazineObj = {};
+  
+   magazineArray.forEach((item) => {
+     if(!magazineObj[item]) magazineObj[item] = 0;
+     magazineObj[item]++;
+   });
+  
+  var isPossible = true;
+  
+   noteArray.forEach((item) => {
+     if (magazineObj[item]) {
+       magazineObj[item]--;
+       if (magazineObj[item] < 0) 
+         isPossible = false;
+     }
+     else {
+       isPossible = false;
+     }
+   })
+   console.log(isPossible);
+ }
+checkMagazine("two times three is not four four Four","two times two is four")
